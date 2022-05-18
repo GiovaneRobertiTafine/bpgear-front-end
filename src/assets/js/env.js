e12bddbbd9d3573623209048bc7f0795cb5f56be
Giovane-Roberti-Tafine <kstv23@hotmail.com>
2022-05-18 13:57:19 -0300

    Alterando fundo login

---
+++ src/assets/js/env.js
@@ -0,0 +1,9 @@
+(function (window) {
+    window.env = window.env || {};
+    window["env"]["cabine_backend_url"] = "http://localhost:5080/jdpi/cabine/api/v1";
+    window["env"]["cabine_worker_url"] = "http://localhost:8088/jdpi/cabine/api/v1";
+    window["env"]["cabine_link_resolucao_bcb31"] = "https://www.bcb.gov.br/estabilidadefinanceira/exibenormativo?tipo=Resolu%C3%A7%C3%A3o%20BCB&numero=31";
+    window["env"]["cabine_link_manual_tempos"] = "https://www.bcb.gov.br/content/estabilidadefinanceira/pix/Regulamento_Pix/IX.ManualdeTemposdoPix-versao2.2.pdf";
+    window["env"]["cabine_funcoes_agencia_conta"] = 'true';
+    window['env']['cabine_funcoes_range_maximo_dias'] = null;
+})(this);
